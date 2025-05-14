65646155b882658655376447264769e0f287fcef
Sirojul Munir <rojulman@gmail.com>
2025-05-14 13:57:35 +0700

    add model unitkerja

---
+++ Models/UnitKerja.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UnitKerja extends Model
+{
+    use HasFactory;
+    protected $table = 'unit_kerjas';
+    protected $fillable = [
+        'nama',
+        'keterangan',
+    ];
+}
