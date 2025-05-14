a2856c1e3b7e531efaa54bfeda64a0814c18c2f4
Sirojul Munir <rojulman@gmail.com>
2025-05-14 14:41:11 +0700

    add route for controller paramedik

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about',function(){
-    return view('about');
-});
-
 Route::get('salam/{name}',function($name){
     return "Assalamualaikum wr.wb $name !!";
 });
 
-use App\Http\Controllers\KelurahanController;
-Route::get('kelurahan', [KelurahanController::class, 'index'])->name('kelurahan.index');
+Route::get('about',function(){
+    return view('about');
+});
+
+Route::get('produk',function(){
+    return view('produk/index');
+});
+
+Route::get('produk/{id}',function($id){
+    return view('produk/index',['idproduk'=>$id]);
+});
+
+use App\Http\Controllers\UnitKerjaController;
+Route::get('unitkerja',[UnitKerjaController::class,'index'])->name('unitkerja.index');
+
+use App\Http\Controllers\ParamedikController;
+Route::get('paramedik',[ParamedikController::class,'index'])->name('paramedik.index');
